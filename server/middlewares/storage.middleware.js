ed9e742f13fd504f063c1cbc273f52fcca060a65
AnkitJaiswal26 <64367253+AnkitJaiswal26@users.noreply.github.com>
2021-07-17 15:25:10 +0530

    Minor change

--- server/middlewares/storage.middleware.js
+++ server/middlewares/storage.middleware.js
@@ -1,8 +1,6 @@
 const multer = require("multer");
 const shortid = require("shortid");
 const path = require("path");
-const multerS3 = require("multer-s3");
-const aws = require("aws-sdk");
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
