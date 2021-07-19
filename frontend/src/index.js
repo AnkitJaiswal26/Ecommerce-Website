bf33d71db0219ca8402e628bc89e5d0568f699cb
TanishSawant <tanishsawant@rediffmail.com>
2021-07-19 20:43:39 +0530

    react-redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import store from './store';
 
+window.store = store;
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store = {}>
+    <React.StrictMode> 
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  
   document.getElementById('root')
 );
 
