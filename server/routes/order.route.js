fc60f61c73067c44da146bc850740a8cbd6604d0
Reshmika Sreenath Nambiar <64072982+Reshmika-Nambiar@users.noreply.github.com>
2021-07-20 01:38:38 +0530

    Added routes for cart and order

---
+++ server/routes/order.route.js
@@ -0,0 +1,17 @@
+import express from 'express'
+const router = express.Router()
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+} from '../controllers/order.controller.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
+
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
+router.route('/myorders').get(protect, getMyOrders)
+router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
+
+export default router
