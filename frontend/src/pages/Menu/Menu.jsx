830a0306381304d4a0afc88ce8052f4cb01f7a02
TanishSawant <tanishsawant@rediffmail.com>
2021-08-02 13:53:21 +0530

    menu_2

--- frontend/src/pages/Menu/Menu.jsx
+++ frontend/src/pages/Menu/Menu.jsx
@@ -3,18 +3,26 @@ import { useHistory, Link, Redirect } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import { useDispatch } from "react-redux";
 import store from "../../store/index";
+import {useSelector} from 'react-redux'
 
 const Menu = () => {
+    const data = useSelector((state) => state.auth);
     const history = useHistory();
-    const state = store.getState();
+    // const state = store.getState();
     const routeChange = (path) => {
         history.push(path);
     }
 
     useEffect(() => {
-        console.log(state.auth.user.email);
-    }, [state])
-    console.log(state)
+        console.log(data);
+    }, [data])
+
+    useEffect(() => {
+        console.log("Hello")
+        console.log(data)
+        
+    }, [data])
+    // console.log(state)
     return (
         <div>
             <h1>Menu</h1>
