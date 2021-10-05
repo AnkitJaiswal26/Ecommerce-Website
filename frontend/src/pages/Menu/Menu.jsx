a9dcb157333b5967258d67efc5727dc6bbe9a144
AnkitJaiswal26 <64367253+AnkitJaiswal26@users.noreply.github.com>
2021-10-05 13:53:07 +0530

    Products

--- frontend/src/pages/Menu/Menu.jsx
+++ frontend/src/pages/Menu/Menu.jsx
@@ -7,6 +7,9 @@ import { useSelector } from "react-redux";
 import { getItems } from "../../actions/products.action";
 import { isAuth } from "../../actions/auth.actions";
 import Topbar from "../../components/Topbar/Topbar";
+import Product from "../../components/Product/Product";
+import "./Menu.scss";
+import Filters from "../../components/Filters/Filters";
 
 const Menu = () => {
 	// const data = useSelector((state) => state);
@@ -38,10 +41,97 @@ const Menu = () => {
 			{isAuth() === false ? <Redirect to="/login" /> : null}
 			<ToastContainer />
 			<Topbar />
+			<Filters />
+			<div className="productsContainer">
+				<Product
+					key="1"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/28.jpg"
+					name="Blue Shirt"
+					price="100.00"
+				/>
+				<Product
+					key="2"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/29.jpg"
+					name="Kids Shirt"
+					price="900.00"
+				/>
+				<Product
+					key="3"
+					name="Blue Jeans"
+					price="560.00"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/28.jpg"
+				/>
+				<Product
+					key="4"
+					name="Lorem Ipsum Kids Six"
+					price="98.90"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/29.jpg"
+				/>
+				<Product
+					key="5"
+					name="Lorem Ipsum Kids Seven"
+					price="100.15"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/28.jpg"
+				/>
+				<Product
+					key="6"
+					name="Lorem Ipsum Kids Ten"
+					price="200.00"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/29.jpg"
+				/>
+				<Product
+					name="White Nice Blue Jacket"
+					price="100"
+					key="7"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/30.jpg"
+				/>
+				<Product
+					key="8"
+					name="Blue Shirt"
+					price="150.00"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/28.jpg"
+				/>
+				<Product
+					key="9"
+					name="White Nice Blue Jacket"
+					price="120.00"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/28.jpg"
+				/>
+				<Product
+					name="Blue Shirt"
+					price="340.00"
+					key="10"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/28.jpg"
+				/>
+				<Product
+					name="Yellow Shoes"
+					price="690.00"
+					key="11"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/28.jpg"
+				/>
+				<Product
+					name="Blue Shirt"
+					price="1050.00"
+					key="12"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/28.jpg"
+				/>
+				<Product
+					key="13"
+					name="Blue Jeans"
+					price="230.00"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/28.jpg"
+				/>
+				<Product
+					key="14"
+					name="Blue Shirt"
+					price="100.00"
+					img="https://flone.reactdemo.hasthemes.com/assets/img/product/fashion/28.jpg"
+				/>
+			</div>
 			{/* <div>
 				<h1>Hello</h1>
 				{/* <h2>{productList[0].name}</h2> */}
-				{/* <ul>
+			{/* <ul>
 					{productList.map((prod) => {
 						return <li>{prod.name}</li>;
 					})}
